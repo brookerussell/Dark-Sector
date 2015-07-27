e794edd36571b7cc17f54d6c8f1925d28f8c7b6c
brookerussell <brooke.russell@yale.edu>
2015-07-27 12:24:58 -0500

    still problems with boosting. bummer

---
+++ SignalMC/VB.h
@@ -0,0 +1,48 @@
+/**
+ * \file VB.h
+ *
+ * \ingroup SignalMC
+ * 
+ * \brief Class def header for a class VB
+ *
+ * @author ber9
+ */
+
+/** \addtogroup SignalMC
+
+    @{*/
+#ifndef VB_H
+#define VB_H
+
+#include <iostream>
+#include <math.h>
+#include <TFile.h>
+#include <TTree.h>
+#include <TLorentzVector.h>
+#include <TVector3.h>
+#include <TGenPhaseSpace.h>
+#include <TH1F.h>
+#include <TH2F.h>
+/**
+   \class VB
+   User defined class VB ... these comments are used to generate
+   doxygen documentation!
+ */
+class VB{
+
+public:
+
+  /// Default constructor
+  VB(){}
+
+  /// Default destructor
+  ~VB(){}
+
+  void PseudoscalarMeson();
+
+  void VectorMeson();
+};
+
+#endif
+/** @} */ // end of doxygen group 
+
